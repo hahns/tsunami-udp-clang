c82fd997bf68a71aed7906e46b0e34798b3ba1b5
ahahn <albert@decisiv.net>
2019-04-22 17:16:04 -0400

    remove windows build info

--- win32compat/sys/socket.h
+++
@@ -1,7 +0,0 @@
-#ifndef __SOCKET_H
-#define __SOCKET_H
-
-#include <winsock2.h>
-#include <sys/ws2tcpip.h>
-
-#endif
--- win32compat/sys/time.h
+++
@@ -1,6 +0,0 @@
-#ifndef __TIME_H
-#define __TIME_H
-
-typedef	long	time_t;
-
-#endif
